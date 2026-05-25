f78bc29d317167fb36d206d9469b90f7415948cb
ritiklonare123 <ritiklonare5@gmail.com>
2026-05-25 16:54:07 +0530

    add h1

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h1> i am ritik</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
